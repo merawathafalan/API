f2102b426224b48a24b65f462a6b0856b5a61947
zakiego <belummikir@gmail.com>
2022-10-08 17:30:44 +0800

    test: add success for juz and surah

--- src/pages/api/v1/state.test.ts
+++ src/pages/api/v1/state.test.ts
@@ -281,12 +281,15 @@ describe("Validation query select", () => {
 });
 
 describe("No Error", () => {
-  test("No Error", async () => {
+  test("Success and correct type mode juz", async () => {
+    const amount = 20;
+    const mode = "juz";
+
     const { req, res } = createMocks({
       method: "GET",
       query: {
-        amount: "10",
-        mode: "juz",
+        amount: amount.toString(),
+        mode,
         select: "12,13,18",
       },
     });
@@ -294,10 +297,85 @@ describe("No Error", () => {
 
     expect(res._getStatusCode()).toBe(200);
     const resp = res._getJSONData();
-    console.log(resp);
-    // expect(resp.error).toEqual({
-    //   type: "query validation error",
-    //   description: "Select tidak boleh kosong",
-    // });
+
+    expect(resp.amount).toBe(amount);
+    expect(resp.mode).toBe(mode);
+    expect(resp.select).toEqual([12, 13, 18]);
+    expect(resp.result.length).toBe(amount);
+
+    for (let i = 0; i < amount; i++) {
+      const sample = resp.result[i];
+
+      expect(sample.id).toBeNumber();
+      expect(sample.question.text).toBeString();
+      expect(sample.question.meta.id).toBeNumber();
+      expect(sample.question.meta.verse_number).toBeNumber();
+      expect(sample.question.meta.verse_key).toBeString();
+      expect(sample.question.meta.juz_number).toBeNumber();
+      expect(sample.question.meta.hizb_number).toBeNumber();
+      expect(sample.question.meta.rub_el_hizb_number).toBeNumber();
+      expect(sample.question.meta.ruku_number).toBeNumber();
+      expect(sample.question.meta.manzil_number).toBeNumber();
+      expect(sample.question.meta.chapter_id).toBeNumber();
+      expect(sample.question.meta.sajdah_number).toBeOneOf([
+        null,
+        expect.toBeNumber(),
+      ]);
+      expect(sample.question.meta.page_number).toBeNumber();
+
+      for (let x = 0; x < sample.options.length; x++) {
+        expect(sample.options[x].value).toBeOneOf([0, 1]);
+        expect(sample.options[x].option).toBeString();
+      }
+    }
+  });
+
+  test("Success and correct type mode surah", async () => {
+    const amount = 20;
+    const mode = "surah";
+
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        amount: amount.toString(),
+        mode,
+        select: "12,13,18",
+      },
+    });
+    await quizHandler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    const resp = res._getJSONData();
+
+    expect(resp.amount).toBe(amount);
+    expect(resp.mode).toBe(mode);
+    expect(resp.select).toEqual([12, 13, 18]);
+    expect(resp.result.length).toBe(amount);
+
+    for (let i = 0; i < amount; i++) {
+      const sample = resp.result[i];
+
+      expect(sample.id).toBeNumber();
+      expect(sample.question.text).toBeString();
+      expect(sample.question.meta.id).toBeNumber();
+      expect(sample.question.meta.verse_number).toBeNumber();
+      expect(sample.question.meta.verse_key).toBeString();
+      expect(sample.question.meta.juz_number).toBeNumber();
+      expect(sample.question.meta.hizb_number).toBeNumber();
+      expect(sample.question.meta.rub_el_hizb_number).toBeNumber();
+      expect(sample.question.meta.ruku_number).toBeNumber();
+      expect(sample.question.meta.manzil_number).toBeNumber();
+      expect(sample.question.meta.chapter_id).toBeNumber();
+      expect(sample.question.meta.sajdah_number).toBeOneOf([
+        null,
+        expect.toBeNumber(),
+      ]);
+      expect(sample.question.meta.page_number).toBeNumber();
+
+      for (let x = 0; x < sample.options.length; x++) {
+        expect(sample.options[x].value).toBeOneOf([0, 1]);
+        expect(sample.options[x].option).toBeString();
+      }
+    }
   });
 });
