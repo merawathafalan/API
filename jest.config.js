6f10b8ba2dc220bc975e270d9ac2416878fdba41
zakiego <belummikir@gmail.com>
2022-10-08 17:30:18 +0800

    feat: setup jest-extended

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ const customJestConfig = {
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/$1",
   },
+  setupFilesAfterEnv: ["jest-extended/all"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
